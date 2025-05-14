4370a10cb6507c003e6f77cb990be72c28e131ba
orionlee3 <64875422+orionlee3@users.noreply.github.com>
2025-05-14 20:38:16 +0000

    pi ge tuning

--- standard_sequences/sweeppiamp.py
+++ standard_sequences/sweeppiamp.py
@@ -318,27 +318,27 @@ def pi_ge_sweep(
     ef_amp = q1.ef_amp
     buffer = 3
     readout_dur = q1.ro_dur
-    mixer_offset_ge=q1.mixer_offset_ge
-    mixer_offset_ef=q1.mixer_offset_ef
+    mixer_offset_ge = q1.mixer_offset_ge
+    mixer_offset_ef = q1.mixer_offset_ef
 
     pi_ge_Q = Pulse(
-        start=file_length - readout_dur ,
+        start=file_length - readout_dur,
         duration=-pi_ge_time,
         amplitude=ge_amp,
         ssm_freq=ssm_ge,
-        phase=90+mixer_offset_ge,
+        phase=90 + mixer_offset_ge,
     )
     ringupdown_seq.add_sweep(channel=4, sweep_name="none", initial_pulse=pi_ge_Q)
 
     pi_ge_I = Pulse(
-        start=file_length - readout_dur ,
+        start=file_length - readout_dur,
         duration=-pi_ge_time,
         amplitude=ge_amp,
         ssm_freq=ssm_ge,
         phase=0,
     )
     ringupdown_seq.add_sweep(channel=1, sweep_name="none", initial_pulse=pi_ge_I)
-    
+
     # HET
     main_pulse = Pulse(
         start=file_length - readout_dur,
@@ -384,7 +384,7 @@ def pi_ge_sweep(
         num_offset=0,
         ch_amp=[1, 1, 1, 1],
     )
-    
+
 
 def pi_ef_sweep(
     q1: object,
@@ -409,20 +409,20 @@ def pi_ef_sweep(
     ef_amp = q1.ef_amp
     buffer = 3
     readout_dur = q1.ro_dur
-    mixer_offset_ge=q1.mixer_offset_ge
-    mixer_offset_ef=q1.mixer_offset_ef
+    mixer_offset_ge = q1.mixer_offset_ge
+    mixer_offset_ef = q1.mixer_offset_ef
 
     pi_ge_Q = Pulse(
-        start=file_length - readout_dur-pi_ef_time ,
+        start=file_length - readout_dur - pi_ef_time,
         duration=-pi_ge_time,
         amplitude=ge_amp,
         ssm_freq=ssm_ge,
-        phase=90+mixer_offset_ge,
+        phase=90 + mixer_offset_ge,
     )
     ringupdown_seq.add_sweep(channel=4, sweep_name="none", initial_pulse=pi_ge_Q)
 
     pi_ge_I = Pulse(
-        start=file_length - readout_dur-pi_ef_time ,
+        start=file_length - readout_dur - pi_ef_time,
         duration=-pi_ge_time,
         amplitude=ge_amp,
         ssm_freq=ssm_ge,
@@ -430,16 +430,16 @@ def pi_ef_sweep(
     )
     ringupdown_seq.add_sweep(channel=1, sweep_name="none", initial_pulse=pi_ge_I)
     pi_ef_Q = Pulse(
-        start=file_length - readout_dur ,
+        start=file_length - readout_dur,
         duration=-pi_ef_time,
         amplitude=ef_amp,
         ssm_freq=ssm_ge,
-        phase=90+mixer_offset_ef,
+        phase=90 + mixer_offset_ef,
     )
     ringupdown_seq.add_sweep(channel=4, sweep_name="none", initial_pulse=pi_ef_Q)
 
     pi_ef_I = Pulse(
-        start=file_length - readout_dur ,
+        start=file_length - readout_dur,
         duration=-pi_ef_time,
         amplitude=ef_amp,
         ssm_freq=ssm_ge,
@@ -491,4 +491,114 @@ def pi_ef_sweep(
         num_offset=0,
         ch_amp=[1, 1, 1, 1],
     )
-    
+
+
+def pi_ge_gaussian_tune_iq(
+    q1,
+    q2,
+    gen_vals,
+    num_steps=3,
+    amp=0.8,
+    pi_ge_time=24,
+    swap_freq=-0.21,
+    swap_time=213.58765318403013,
+    file_length=16000,
+    verbose=True,
+):
+    ringupdown_seq = Sequence(file_length, num_steps)
+
+    ssm_ge = q1.ge_ssm
+    mixer_offset_ge = q1.mixer_offset_ge
+    readout_amp_1 = q1.ro_amp
+    ROIF1 = q1.ROIF
+    readout_amp_2 = q2.ro_amp
+    ROIF2 = q2.ROIF
+    readout_dur = q1.ro_dur
+    buffer = 3
+
+    # Loop to add 4 pi pulses (I and Q channels)
+    for i in range(4):
+        start_time = (
+            file_length
+            - readout_dur
+            - (4 - i) * buffer
+            - (3 - i) * pi_ge_time
+            - swap_time
+        )
+        pi_ge_I = Pulse(
+            start=start_time,
+            duration=-pi_ge_time,
+            amplitude=amp,
+            ssm_freq=ssm_ge,
+            phase=0,
+            gaussian_bool=True,
+        )
+        pi_ge_Q = Pulse(
+            start=start_time,
+            duration=-pi_ge_time,
+            amplitude=amp,
+            ssm_freq=ssm_ge,
+            phase=90 + mixer_offset_ge,
+            gaussian_bool=True,
+        )
+        ringupdown_seq.add_sweep(channel=1, sweep_name="none", initial_pulse=pi_ge_I)
+        ringupdown_seq.add_sweep(channel=4, sweep_name="none", initial_pulse=pi_ge_Q)
+
+    # Swap pulse
+    swap = Pulse(
+        start=file_length - readout_dur,
+        duration=-swap_time,
+        amplitude=1.36,
+        ssm_freq=swap_freq,
+        phase=0,
+    )
+    ringupdown_seq.add_sweep(channel=3, sweep_name="none", initial_pulse=swap)
+
+    # Readout pulses for both qubits
+    for amp, ROIF in [(readout_amp_1, ROIF1), (readout_amp_2, ROIF2)]:
+        readout = Pulse(
+            start=file_length - readout_dur,
+            duration=readout_dur,
+            amplitude=amp,
+            ssm_freq=ROIF,
+            phase=-file_length * ROIF * 360,
+        )
+        ringupdown_seq.add_sweep(channel=2, sweep_name="none", initial_pulse=readout)
+
+    # Alazar trigger
+    alazar_trigger = Pulse(
+        start=file_length - readout_dur - 1000, duration=1000, amplitude=1
+    )
+    ringupdown_seq.add_sweep(
+        channel=3, marker=1, sweep_name="none", initial_pulse=alazar_trigger
+    )
+
+    # Plot if verbose
+    if verbose:
+        ch1, ch3, m1 = (
+            ringupdown_seq.channel_list[0][0],
+            ringupdown_seq.channel_list[2][0],
+            ringupdown_seq.channel_list[0][2],
+        )
+        full = ch1 + ch3 + m1
+        plt.figure()
+        plt.imshow(
+            full[
+                :, file_length - readout_dur - 1000 - 4000 : file_length - readout_dur
+            ],
+            aspect="auto",
+        )
+        plt.show()
+
+    # Save and load the sequence
+    write_dir = (
+        r"C:\arbsequences\strong_dispersive_withPython\test_pulse_ringupdown_bin"
+    )
+    ringupdown_seq.write_sequence_to_disk(
+        "foo", write_dir, use_range_01=False, num_offset=0, write_binary=True
+    )
+    ringupdown_seq.load_sequence_from_disk(
+        "128.252.134.31", "foo", write_dir, num_offset=0, ch_amp=[1, 1, 1, 1]
+    )
+
+    return ringupdown_seq
